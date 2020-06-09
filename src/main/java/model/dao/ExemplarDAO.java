ade5011d5c572e2a7f8ea341ab93a7b67b8fb38b
DexterFLN <dragon_dexter-x@yahoo.com.br>
2020-06-08 21:34:24 -0300

    Conclusão do ExemplarDAO com LivroDAO e override toString

--- src/main/java/model/dao/ExemplarDAO.java
+++ src/main/java/model/dao/ExemplarDAO.java
@@ -10,7 +10,14 @@ import model.vo.Exemplar;
 import model.vo.Livro;
 
 public class ExemplarDAO {
-
+	
+	//TODO criar construção de resultSet para ALUGUEL
+	//TODO criar construção de resultSet para LIVRO
+	//TODO criar construção de resultSet para EXEMPLAR
+	
+	//TODO criar mais métodos de consulta de EXEMPLAR (consulta vinda de um aluguel, e vinda de um livro)
+	// CONSULTA DE ALUGUEL "SELECT * FROM EXEMPLAR INNER JOIN ALUGUEL ON EXEMPLAR.id = ALUGUEL.idExemplar WHERE idLivro=1;"
+	
 	public Exemplar construirExemplarDoResultSet(ResultSet resultSet) {
 		Exemplar exemplar = new Exemplar();
 
@@ -28,29 +35,20 @@ public class ExemplarDAO {
 		return exemplar;
 	}
 
-	public Exemplar consultarExemplar(int id) {
-		Connection connection = Banco.getConnection();
-		String sql = "SELECT * FROM EXEMPLAR WHERE id=?";
-		PreparedStatement preparedStatement = Banco.getPreparedStatement(connection, sql,
-				PreparedStatement.RETURN_GENERATED_KEYS);
-		ResultSet resultSet = null;
-		Exemplar exemplar = null;
+	
+	public Exemplar construirExemplaresDeLivroDoResultSet(ResultSet resultSet) {
+		Exemplar exemplar = new Exemplar();
 
 		try {
-			preparedStatement.setInt(1, id);
-			resultSet = preparedStatement.executeQuery();
+			exemplar.setId(resultSet.getInt("id"));
 
-			if (resultSet != null && resultSet.next()) {
-				exemplar = construirExemplarDoResultSet(resultSet);
-			}
+			LivroDAO livroDAO = new LivroDAO();
+			Livro livro = livroDAO.consultarLivroPorIdParaExemplares(resultSet.getInt("idLivro"));
+			exemplar.setLivro(livro);
 		} catch (SQLException ex) {
-			System.out.println("Erro ao consultar exemplar.");
+			System.out.println("Erro ao construir exemplar do resultSet.");
 			System.out.println("Erro: " + ex.getMessage());
-		} finally {
-			Banco.closeResultSet(resultSet);
-			Banco.closePreparedStatement(preparedStatement);
-			Banco.closeConnection(connection);
-		}
+		} 
 
 		return exemplar;
 	}
@@ -68,7 +66,7 @@ public class ExemplarDAO {
 			resultSet = preparedStatement.executeQuery();
 
 			while (resultSet.next()) {
-				Exemplar exemplar = construirExemplarDoResultSet(resultSet);
+				Exemplar exemplar = construirExemplaresDeLivroDoResultSet(resultSet);
 				exemplares.add(exemplar);
 			}
 		} catch (SQLException ex) {
