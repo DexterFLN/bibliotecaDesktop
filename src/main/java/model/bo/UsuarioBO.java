380ff658dfcedee73d3b52acfd8b4baeed5f53d3
DexterFLN <dragon_dexter-x@yahoo.com.br>
2020-06-28 20:01:51 -0300

    Ajuste com a remoção de atributos não utilizados e inclusão da coluna cpf no painel de consulta (ainda não finalizado)

--- src/main/java/model/bo/UsuarioBO.java
+++ src/main/java/model/bo/UsuarioBO.java
@@ -1,27 +1,25 @@
 package model.bo;
 
 import java.util.ArrayList;
-
 import model.dao.UsuarioDAO;
 import model.seletor.UsuarioSeletor;
-import model.vo.Endereco;
 import model.vo.Usuario;
 import util.GeradorPlanilha;
 
 public class UsuarioBO {
-	private UsuarioDAO usuarioDAO = new UsuarioDAO();
+	
 	public ArrayList<Usuario> consultarUsuarioPorFiltro(UsuarioSeletor usuarioSeletor) {
-		return usuarioDAO.consultarUsuarioPorFiltro(usuarioSeletor);
+		return UsuarioDAO.consultarUsuarioPorFiltro(usuarioSeletor);
 	}
 	public Boolean alterarUsuario(Usuario usuarioAlterado) {
-		return usuarioDAO.alterar(usuarioAlterado);
+		return UsuarioDAO.alterar(usuarioAlterado);
 	}
 	
 	public void gerarRelatorio(ArrayList<Usuario> usuarios, String caminhoEscolhido) {
 		 GeradorPlanilha.gerarPlanilhaUsuarios(usuarios, caminhoEscolhido);
 	}
 	public Usuario cadastrarUsuario(Usuario usuario) {
-		return usuarioDAO.salvar(usuario);
+		return UsuarioDAO.salvar(usuario);
 		 
 	}
 	public static boolean existeUsuarioPorCpf(Usuario usuario) {
